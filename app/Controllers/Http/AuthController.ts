5760b2ec4d7efd201e3ddc291d520dbd08244b0d
lmeniconi <luciano@asimov.cl>
2022-11-06 01:57:41 -0300

    init

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,90 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Env from '@ioc:Adonis/Core/Env'
+import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
+
+import User from 'App/Models/User'
+
+const FRONTEND_REDIRECT_URI = `${Env.get('FRONTEND_URL')}/auth/callback`
+
+export default class AuthController {
+  public async me({ auth }: HttpContextContract) {
+    return auth.user
+  }
+
+  public async auth0({ response, session }: HttpContextContract) {
+    const state = uuidv4()
+    session.put('state', state)
+
+    response.redirect(
+      `${Env.get('AUTH0_DOMAIN')}/authorize?` +
+        new URLSearchParams({
+          client_id: Env.get('AUTH0_CLIENT_ID'),
+          redirect_uri: Env.get('APP_URL') + '/authorized/auth0',
+          response_type: 'code',
+          scope: 'openid profile email',
+          state,
+        }).toString()
+    )
+  }
+
+  public async auth0Callback({ auth, request, response, session }: HttpContextContract) {
+    const { state } = request.all()
+    const sessionState = session.get('state')
+
+    if (state !== sessionState) return response.badRequest({ message: 'Invalid state' })
+
+    const { code } = request.all()
+    let res = await axios.post(`${Env.get('AUTH0_DOMAIN')}/oauth/token`, {
+      client_id: Env.get('AUTH0_CLIENT_ID'),
+      client_secret: Env.get('AUTH0_CLIENT_SECRET'),
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: Env.get('APP_URL') + '/authorized',
+    })
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { access_token } = res.data
+
+    res = await axios.get(`${Env.get('AUTH0_DOMAIN')}/userinfo`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+
+    const { data } = res
+
+    console.log(data)
+
+    let user = await User.findBy('email', data.email)
+    if (!user) {
+      user = new User()
+      user.role = 'user'
+    }
+
+    user.email = data.email
+    user.firstName = data.given_name
+    user.lastName = data.family_name
+    user.picture = data.picture
+    user.provider = 'auth0'
+    await user.save()
+
+    await auth.login(user)
+    response.redirect(FRONTEND_REDIRECT_URI)
+  }
+
+  public async auth0Logout({ response }) {
+    response.redirect(
+      `${Env.get('AUTH0_DOMAIN')}/v2/logout?` +
+        new URLSearchParams({
+          client_id: Env.get('AUTH0_CLIENT_ID'),
+          returnTo: Env.get('APP_URL') + '/logout/auth0/callback',
+        }).toString()
+    )
+  }
+
+  public async auth0LogoutCallback({ auth, response }) {
+    await auth.logout()
+    response.redirect(Env.get('FRONTEND_URL'))
+  }
+}
---
+++ app/Models/User.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { column, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column()
+  public firstName: string
+
+  @column()
+  public lastName: string
+
+  @column()
+  public picture: string | null
+
+  @column()
+  public provider: 'auth0'
+
+  @column()
+  public role: 'admin' | 'user'
+
+  @column()
+  public rememberMeToken?: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
